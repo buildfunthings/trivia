1844ad3a1740ed71c05295447ef031c3288717a3
Arjen Wiersma <arjen@wiersma.org>
2016-09-16 11:19:55 +0200

    Add a working wireframe

--- src/cljs/trivia/subs.cljs
+++ src/cljs/trivia/subs.cljs
@@ -1,11 +1,20 @@
 (ns trivia.subs
   (:require [re-frame.core :as re-frame]))
 
+(re-frame/reg-sub
+ :state
+ (fn [db _]
+   (:state db)))
 
 (re-frame/reg-sub
  :current-question
  (fn [db _]
    (:current-question db)))
+
+(re-frame/reg-sub
+ :answer-state
+ (fn [db _]
+   (:answer-state db)))
 
 (re-frame/reg-sub
  :name
