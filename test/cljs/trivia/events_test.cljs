35af4e6c8e4ddcccd0a98cdb7dcc7c3efd5b44a0
Arjen Wiersma <arjen@wiersma.org>
2016-09-23 12:18:11 +0200

    Fixed the . in the namespace declaration

--- test/cljs/trivia/events_test.cljs
+++ test/cljs/trivia/events_test.cljs
@@ -1,6 +1,7 @@
-(ns .trivia.events-test
+(ns trivia.events-test
   (:require [clojure.test :as t]
             [re-frame.core :as rf]
             [trivia.events :as sut]))
 
-(rf/register-handler :submit-answer [] sut)
+;;(rf/register-handler :submit-answer [] sut)
+
